cdedcac25eacc10cb5f2aa1324a5cb3cb0aac80d
ckaplan19 <72571904+ckaplan19@users.noreply.github.com>
2022-07-27 12:58:42 +0300

    Add files via upload

---
+++ p3_64229_aakyurek17_71868_ckaplan19/comp301_project3 2/project3_questions/Project3Part3/lang.scm
@@ -0,0 +1,81 @@
+(module lang
+
+  ;; grammar for the LEXADDR language
+
+  (lib "eopl.ss" "eopl")                
+  
+  (require "drscheme-init.scm")
+  
+  (provide (all-defined-out))
+
+  ;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+  
+  (define the-lexical-spec
+    '((whitespace (whitespace) skip)
+      (comment ("%" (arbno (not #\newline))) skip)
+      (identifier
+       (letter (arbno (or letter digit "_" "-" "?")))
+       symbol)
+      (number (digit (arbno digit)) number)
+      (number ("-" digit (arbno digit)) number)
+      
+      ))
+  
+  (define the-grammar
+    '((program (expression) a-program)
+
+      (expression (number) const-exp)
+      (expression
+        ("-" "(" expression "," expression ")")
+        diff-exp)
+      
+      (expression
+       ("zero?" "(" expression ")")
+       zero?-exp)
+
+      (expression
+       ("if" expression "then" expression "else" expression)
+       if-exp)
+
+      (expression (identifier) varr-exp)
+
+      (expression
+       ("let" identifier "=" expression "in" expression)
+       lett-exp)   
+
+      (expression
+       ( "proc" "(" identifier ")" expression)
+       procc-exp)
+
+      (expression
+       ("(" expression expression ")")
+       call-exp)
+
+      (expression ("%nameless-var" identifier) var-exp)
+      (expression
+        ("%let" identifier expression "in" expression )
+        let-exp)
+      (expression
+        ("%lexproc" identifier expression)
+        proc-exp)
+
+      (expression
+       ("aa")
+       dummy-exp)
+      
+      ))
+
+  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+  
+  (sllgen:make-define-datatypes the-lexical-spec the-grammar)
+  
+  (define show-the-datatypes
+    (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+  
+  (define scan&parse
+    (sllgen:make-string-parser the-lexical-spec the-grammar))
+  
+  (define just-scan
+    (sllgen:make-string-scanner the-lexical-spec the-grammar))
+  
+  )
